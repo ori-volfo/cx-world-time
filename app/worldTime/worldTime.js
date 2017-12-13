1c68bb1a59f694889b3d5b395aaea2ae21b16eff
ori-volfo <ori.volfo@gmail.com>
2017-12-13 14:25:34 +0200

    cellexpert test part2

--- app/worldTime/worldTime.js
+++ app/worldTime/worldTime.js
@@ -13,17 +13,20 @@ angular.module('myApp.worldTime', ['ngRoute'])
     var gmtOffset = 0;
     $scope.format = "yyyy-M-d HH:mm:ss";
     $scope.zones = [];
+    $scope.zonesPopulated = false;
     $scope.time = new Date();
+    var UTCoffset = (new Date).getTimezoneOffset() * 60 * 1000;
 
     $interval(function () {
         var now = new Date().getTime();
-        now += gmtOffset*1000;
+        now += gmtOffset*1000 + UTCoffset;
         var timeRaw = new Date(now);
         $scope.time = timeRaw;
     }, 1000);
 
     $http.get('http://dev.cellxpert.com/test/worldtime/').then(function(response){
         $scope.zones = response.data.zones;
+        $scope.zonesPopulated = true;
     });
 
     $scope.selectCountry = function(index){
